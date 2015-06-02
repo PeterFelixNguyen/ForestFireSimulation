118d96929adaabef63610513f37c4a415efaa826
Peter "Felix" Nguyen <PeterFelixNguyen@gmail.com>
2015-06-02 16:34:40 -0700

    LookAndFeel class to streamline UI design.

---
+++ src/LookAndFeel.java
@@ -0,0 +1,7 @@
+import java.awt.Color;
+
+public class LookAndFeel {
+	public static final Color COLOR_PANELS = new Color(215,199,151);
+	public static final Color COLOR_BLACK = new Color(0,0,0);
+	public static final Color COLOR_WHITE = new Color(255,255,255);
+}
