5847356bb9f395587d44bb0054eddd46fabf2e65
Peter "Felix" Nguyen <PeterFelixNguyen@gmail.com>
2015-06-04 20:39:48 -0700

    Copyright + description added. Added new Color.

--- src/LookAndFeel.java
+++ src/LookAndFeel.java
@@ -1,7 +1,19 @@
+/**
+ * Copyright 2015 Peter "Felix" Nguyen & Emmanuel Medina Lopez
+ * 
+ * Forest Fire Simulation with 2D Graphics.
+ * 
+ * LookAndFeel is a class that provides GUI "Look and Feel" constants 
+ * to streamline the implementation of common GUI characterstics.
+ * 
+ * Note: this class could potentially be expanded with 
+ *       implementations to change application theme 
+ */
 import java.awt.Color;
 
 public class LookAndFeel {
-	public static final Color COLOR_PANELS = new Color(215,199,151);
-	public static final Color COLOR_BLACK = new Color(0,0,0);
-	public static final Color COLOR_WHITE = new Color(255,255,255);
+	public static final Color COLOR_SOLID_PANELS = new Color(215,199,151);
+	public static final Color COLOR_SOLID_BLACK = new Color(0,0,0);
+	public static final Color COLOR_SOLID_WHITE = new Color(255,255,255);
+	public static final Color COLOR_TRANSLUCENT_BLACK = new Color(0,0,0,.5f);
 }
